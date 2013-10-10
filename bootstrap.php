e52f2095f023e92e305ba294ce430a3e8ba9fd51
Andrew Patton <andrew@acusti.ca>
2013-10-10 13:31:07 -0400

    Add explanation, remove everything else
    As explained in the comment, using PHP Composer to install Solarium means we don’t need to bootstrap anything

--- bootstrap.php
+++ bootstrap.php
@@ -3,14 +3,10 @@
 /**
  * Solr: Solr wrapper for FuelPHP
  *
- * @package    Solr
- * @version    v0.0
- * @author     Ben Grimes
- * @license    MIT License
+ * @package	  FuelPHP Solarium
+ * @version	  v0.0.1
+ * @author	  Andrew Patton
+ * @license	  MIT License
  */
 
-
-require('solarium/Autoloader.php');
-Solarium_Autoloader::register();
-
-/* End of file bootstrap.php */
+// We do not need to do anything here, because FuelPHP's core bootstrap.php uses Composer's autoload.php which loads Solarium for us
