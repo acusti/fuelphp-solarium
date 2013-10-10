f85d599e8fcdd039c3c517e3bb5a444a5ce39dff
Andrew Patton <andrew@acusti.ca>
2013-10-10 13:22:15 -0400

    Update config to be Solarium 3.x compatible
    Also, previously, it was not functional (in my testing)

--- config/solarium.php
+++ config/solarium.php
@@ -1,21 +1,22 @@
 <?php
 
 /**
- * Solr: Solr wrapper for FuelPHP
+ * Solarium: Integration of Solarium into FuelPHP by providing config and calling Autoloader::register (Solarium library is included in /vendor via PHP Composer)
  *
- * @package    Solr
- * @version    v0.0
- * @author     Ben Grimes
- * @license    MIT License
+ * @package	  FuelPHP Solarium
+ * @version	  v0.0.1
+ * @author	  Andrew Patton
+ * @license	  MIT License
  */
 
 return array(
-    // Server information
-    'default' => array(
-        'url'         => 'http://127.0.0.1',
-        'port'        => 8080,
-        'application' => 'default',
-    ),
+	// Endpoints (use to define multiple solr servers or cores)
+	'endpoint' => array(
+		// Server information
+        'localhost' => array(
+            'host' => '127.0.0.1',
+            'port' => 8983,
+            'path' => '/solr/',
+        ),
+	)
 );
-
-/* End of file config/solr.php */
